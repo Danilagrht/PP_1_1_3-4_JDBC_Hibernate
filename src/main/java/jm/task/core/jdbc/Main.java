5b24acb2d7cb8aedfa4f9836948ea6d384a7cd29
danila smyshlyaev <d.smyshlyaev@kodland.team>
2023-12-18 18:49:48 +0300

    user table created

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,32 @@
 package jm.task.core.jdbc;
 
+import java.sql.*;
+
 public class Main {
+
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/pp_1_1_4";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Ohdude0505Bdhjl11721";
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        // Устанавливаем соединение с базой данных
+        try (Connection connection = DriverManager.getConnection(JDBC_URL, USER, PASSWORD)) {
+
+            try (Statement statement = connection.createStatement()) {
+                String createTableSQL = "CREATE TABLE IF NOT EXISTS User (" +
+                        "id BIGINT PRIMARY KEY AUTO_INCREMENT, " +
+                        "name VARCHAR(255), " +
+                        "lastName VARCHAR(255)," +
+                        "age SMALLINT" +
+                        ")";
+
+                statement.execute(createTableSQL);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
