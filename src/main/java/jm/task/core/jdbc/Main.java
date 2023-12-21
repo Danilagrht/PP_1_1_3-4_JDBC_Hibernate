18c08f3155843cc27a1dbee48ccd6a48faec5efb
danila smyshlyaev <d.smyshlyaev@kodland.team>
2023-12-21 16:40:29 +0300

    sql statements added, override User.toString

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,5 +1,7 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+
 import java.sql.*;
 
 public class Main {
@@ -9,23 +11,61 @@ public class Main {
     private static final String PASSWORD = "Ohdude0505Bdhjl11721";
 
     public static void main(String[] args) {
-        // Устанавливаем соединение с базой данных
-        try (Connection connection = DriverManager.getConnection(JDBC_URL, USER, PASSWORD)) {
+        User[] users = {
+                new User("Adele", "Adkins", (byte) 35),
+                new User("Bob", "Dylan", (byte) 82),
+                new User("Chad", "Kroeger", (byte) 49),
+                new User("Dave", "Grohl", (byte) 54)
+        };
 
-            try (Statement statement = connection.createStatement()) {
-                String createTableSQL = "CREATE TABLE IF NOT EXISTS User (" +
-                        "id BIGINT PRIMARY KEY AUTO_INCREMENT, " +
-                        "name VARCHAR(255), " +
-                        "lastName VARCHAR(255)," +
-                        "age SMALLINT" +
-                        ")";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            Connection connection = DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
 
-                statement.execute(createTableSQL);
-            } catch (SQLException e) {
-                e.printStackTrace();
+            //подготавливаю SQL запросы
+            PreparedStatement createUserTable = connection.prepareStatement(
+                    "CREATE TABLE IF NOT EXISTS User (" +
+                    "id BIGINT PRIMARY KEY AUTO_INCREMENT," +
+                    "name VARCHAR(255), " +
+                    "lastName VARCHAR(255)," +
+                    "age SMALLINT" +
+                    ")");
+
+            PreparedStatement addUser = connection.prepareStatement(
+                    "INSERT INTO User (name, lastName, age)\n" +
+                    "VALUES (?, ?, ?)");
+
+            PreparedStatement getAllUsers = connection.prepareStatement(
+                    "SELECT * FROM User");
+
+            PreparedStatement deleteAllUsers = connection.prepareStatement(
+                    "DELETE FROM User");
+
+            PreparedStatement deleteUserTable = connection.prepareStatement(
+                    "DROP TABLE IF EXISTS User");
+
+            //выполняю операции
+            createUserTable.executeUpdate();
+
+            for (User user : users) {
+                addUser.setString(1, user.getName());
+                addUser.setString(2, user.getLastName());
+                addUser.setByte(3, user.getAge());
+                addUser.executeUpdate();
+                System.out.printf("User с именем – %s добавлен в базу данных\n", user.getName());
             }
 
-        } catch (SQLException e) {
+            ResultSet resultSet = getAllUsers.executeQuery();
+            while(resultSet.next()){
+                User user = new User(resultSet.getString(2), resultSet.getString(3), resultSet.getByte(4));
+                user.setId(resultSet.getLong(1));
+                System.out.println(user);
+            }
+
+            deleteAllUsers.executeUpdate();
+
+            deleteUserTable.executeUpdate();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -59,4 +59,9 @@ public class User {
     public void setAge(Byte age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "id: " + this.id + ", name: " + this.name + ", lastName: " + this.lastName + ", age: " + this.age;
+    }
 }
